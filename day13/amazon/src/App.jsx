6d7d70e687c78b08adcf1c378735b15452bef7ac
abhishek-123-code <your-email@example.com>
2025-11-13 15:23:23 +0530

    Add day13 folder with amazon Vite project

---
+++ day13/amazon/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './App.css'
+import Counter from './Count'
+
+
+function App() {
+
+  
+  const images = [
+   
+  ]
+  
+  const [index, setIndex] = useState(0);
+
+  const prev = () => setIndex((index - 1 + images.length) % images.length);
+
+  const next = () => setIndex((index + 1) % images.length);
+
+  return (
+    <>
+      <div className="img-slider">
+        <h1>Image Slider</h1>
+        <img src={images[index]} alt="image-here" />
+        <div className="slider-btn">
+            <button onClick={prev}>left</button>
+            <button onClick={next}>right</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
