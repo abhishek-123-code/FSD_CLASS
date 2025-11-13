56fcee31cf99c0ce1be00bde7a9a2b22d47fdd37
abhishek-123-code <your-email@example.com>
2025-11-13 15:13:58 +0530

    day5 javascript_intro

---
+++ day5/javascript/Index.js
@@ -0,0 +1,34 @@
+//1part console.log("hello world");
+// document.writeln("welcome to the FSD Session");
+// document.writeln("welcome to the FSD Session");
+// document.writeln("welcome to the FSD Session");
+// alert("Wrong Password");
+// let a = prompt("Enter value of a"); 
+
+
+//2part console.log(a);
+// var a = 20;
+// console.log(a);
+// var a = 30;
+// console.log(a);
+// {
+//     var a = 40;
+//     console.log(a);
+// }
+// fn(80);
+// function fn(a) {
+//     console.log(a);
+//     var a = 90;
+//     console.log(a);
+// }
+
+
+//3part
+// let a = -5 + 5 + 22 + "22";
+// let b = "22" + 22 + 5- 5;
+// console.log("value of a is "+ a + " and type of a is "+ typeof(a));
+// console.log("value of b is "+ b + " and type of b is "+ typeof(b));
+
+
+
+//4 part
